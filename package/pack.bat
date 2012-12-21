9c3f7b116dfca67193e3bc796e5927b8ce9e3e43
Teun Duynstee <github@duynstee.com>
2012-12-21 22:58:14 +0100

    Create net35 and net40 binaries from one .bat

--- package/pack.bat
+++ package/pack.bat
@@ -1,1 +1,3 @@
-"c:\Program Files\NuGet 1.6\nuget" Pack BatchFlow.0.5.nuspec
+"c:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" ..\BatchFlow\BatchFlow.csproj /p:Configuration=Release35
+"c:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" ..\BatchFlow\BatchFlow.csproj /p:Configuration=Release40
+"c:\Program Files\NuGet 1.6\nuget" Pack BatchFlow.nuspec
